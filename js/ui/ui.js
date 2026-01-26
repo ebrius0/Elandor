8fb41d9c461ec52679313b5bcac32ab70bc896c0
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:38:56 +0100

    Aktualisieren von ui.js

--- js/ui/ui.js
+++ js/ui/ui.js
@@ -1,1 +1,11 @@
-1
+import { GameState } from "../core/gamestate.js";
+import { getPrice } from "../core/economy.js";
+
+export function render() {
+  const out = document.getElementById("output");
+  out.innerHTML = `
+    <p>Tag ${GameState.player.tag}</p>
+    <p>Gold: ${GameState.player.gold}</p>
+    <p>Stadt: ${GameState.player.aktuelleStadt}</p>
+  `;
+}
