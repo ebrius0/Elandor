054e602ae50e2ab8223db87f903e86f957df0fcd
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:38:19 +0100

    Aktualisieren von combat.js

--- js/core/combat.js
+++ js/core/combat.js
@@ -1,1 +1,11 @@
-1
+import { GameState } from "./gamestate.js";
+
+export function pirateCheck() {
+  if (Math.random() < 0.05) {
+    return {
+      text: "Piraten greifen an!",
+      success: Math.random() > 0.5
+    };
+  }
+  return null;
+}
