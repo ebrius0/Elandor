402e4a43f0988e193eff2022adf36e7a0423e3cb
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:36:49 +0100

    Aktualisieren von data.js

--- js/core/data.js
+++ js/core/data.js
@@ -1,1 +1,25 @@
-1
+export const Cities = {
+  Aethelgard: {
+    versorgung: "Gut",
+    verbindungen: ["Port Valerius"],
+    gueter: {
+      Getreide: { basisPreis: 10, lager: 80, produktion: 0.7 },
+      Fisch: { basisPreis: 12, lager: 60, produktion: 0.8 }
+    }
+  }
+};
+
+export const Goods = [
+  "Getreide", "Fisch", "Holz", "Eisen", "Waffen"
+];
+
+export const ShipTypes = {
+  Schnigge: { kapazitaet: 50, speed: 1, waffenPlaetze: 1 },
+  Kraier: { kapazitaet: 150, speed: 1.1, waffenPlaetze: 2 }
+};
+
+export const Buildings = {
+  Werft: { max: 3 },
+  Lagerhaus: { max: 3 },
+  Kontor: { max: 3 }
+};
