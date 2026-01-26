cd755f36a3cbe07d26429a1f54b8a1d21d29df94
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:39:21 +0100

    Aktualisieren von main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-1
+import { GameState } from "./core/gamestate.js";
+import { render } from "./ui/ui.js";
+
+GameState.load();
+GameState.init();
+render();
+
+setInterval(() => {
+  GameState.nextDay();
+  render();
+  GameState.save();
+}, 30000);
