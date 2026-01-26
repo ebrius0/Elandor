453a3040cb294dbe60452e7c7a791ceb83f7dc1d
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:37:55 +0100

    Aktualisieren von travel.js

--- js/core/travel.js
+++ js/core/travel.js
@@ -1,1 +1,9 @@
-1
+import { Cities } from "./data.js";
+
+export function canTravel(from, to) {
+  return Cities[from].verbindungen.includes(to);
+}
+
+export function travelTime(from, to) {
+  return Math.max(1, Math.floor(Math.random() * 3) + 1);
+}
