cbdf852a1646baa011ddcc67fbae9f99adde3303
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:53:16 +0100

    Aktualisieren von data.js

--- js/core/data.js
+++ js/core/data.js
@@ -1,25 +1,104 @@
 export const Cities = {
   Aethelgard: {
     versorgung: "Gut",
-    verbindungen: ["Port Valerius"],
+    einwohner: 1200,
     gueter: {
-      Getreide: { basisPreis: 10, lager: 80, produktion: 0.7 },
-      Fisch: { basisPreis: 12, lager: 60, produktion: 0.8 }
-    }
+      Getreide: { basisPreis: 10, lager: 80 },
+      Fisch: { basisPreis: 12, lager: 50 }
+    },
+    gebaeude: ["Werft", "Taverne", "Kontor"]
+  },
+
+  PortValerius: {
+    name: "Port Valerius",
+    versorgung: "Sehr gut",
+    einwohner: 1800,
+    gueter: {
+      Holz: { basisPreis: 15, lager: 70 },
+      Tuch: { basisPreis: 25, lager: 40 }
+    },
+    gebaeude: ["Werft", "Taverne", "Kontor"]
+  },
+
+  Silberhafen: {
+    versorgung: "Mittel",
+    einwohner: 900,
+    gueter: {
+      Eisen: { basisPreis: 30, lager: 40 },
+      Silbererz: { basisPreis: 45, lager: 25 }
+    },
+    gebaeude: ["Taverne", "Kontor"]
+  },
+
+  Drachenbucht: {
+    versorgung: "Hervorragend",
+    einwohner: 1500,
+    gueter: {
+      Wein: { basisPreis: 20, lager: 50 },
+      Seide: { basisPreis: 80, lager: 10 }
+    },
+    gebaeude: ["Werft", "Taverne", "Kontor"]
+  },
+
+  Sonnenkueste: {
+    name: "Sonnenküste",
+    versorgung: "Gut",
+    einwohner: 1100,
+    gueter: {
+      Gewuerze: { basisPreis: 60, lager: 20 },
+      Fruechte: { basisPreis: 40, lager: 30 }
+    },
+    gebaeude: ["Taverne", "Kontor"]
+  },
+
+  Sturmkap: {
+    versorgung: "Sehr gut",
+    einwohner: 1400,
+    gueter: {
+      Waffen: { basisPreis: 70, lager: 15 },
+      Eisen: { basisPreis: 30, lager: 35 }
+    },
+    gebaeude: ["Werft", "Taverne"]
+  },
+
+  Gezeitenruh: {
+    versorgung: "Schlecht",
+    einwohner: 600,
+    gueter: {
+      Rum: { basisPreis: 80, lager: 20 },
+      Salz: { basisPreis: 18, lager: 30 }
+    },
+    gebaeude: ["Taverne"]
+  },
+
+  Wolkenfels: {
+    versorgung: "Gut",
+    einwohner: 750,
+    gueter: {
+      Edelsteine: { basisPreis: 120, lager: 5 },
+      Perlen: { basisPreis: 60, lager: 15 }
+    },
+    gebaeude: ["Kontor"]
+  },
+
+  Auenbrueck: {
+    name: "Auenbrück",
+    versorgung: "Schlecht",
+    einwohner: 800,
+    gueter: {
+      Holz: { basisPreis: 15, lager: 60 },
+      Kohle: { basisPreis: 20, lager: 50 }
+    },
+    gebaeude: ["Taverne", "Kontor"]
+  },
+
+  Flussfurt: {
+    versorgung: "Mittel",
+    einwohner: 700,
+    gueter: {
+      Getreide: { basisPreis: 10, lager: 90 },
+      Leder: { basisPreis: 22, lager: 25 }
+    },
+    gebaeude: ["Kontor"]
   }
-};
-
-export const Goods = [
-  "Getreide", "Fisch", "Holz", "Eisen", "Waffen"
-];
-
-export const ShipTypes = {
-  Schnigge: { kapazitaet: 50, speed: 1, waffenPlaetze: 1 },
-  Kraier: { kapazitaet: 150, speed: 1.1, waffenPlaetze: 2 }
-};
-
-export const Buildings = {
-  Werft: { max: 3 },
-  Lagerhaus: { max: 3 },
-  Kontor: { max: 3 }
 };
