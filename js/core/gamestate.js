17d7b0707daf9887e93319f569dd194c218639fd
ebrius0 <ebriuslaptop@gmail.com>
2026-01-26 20:33:36 +0100

    Aktualisieren von gamestate.js

--- js/core/gamestate.js
+++ js/core/gamestate.js
@@ -1,1 +1,35 @@
-#Spieler
+export const GameState = {
+  player: {
+    gold: 5000,
+    level: 1,
+    xp: 0,
+    ruf: 0,
+    tag: 1,
+    aktuelleStadt: "Aethelgard",
+    inventar: {},
+    schiffe: []
+  },
+
+  init() {
+    this.player.schiffe.push({
+      typ: "Schnigge",
+      ort: "Aethelgard",
+      status: "anker",
+      ladung: {},
+      waffen: 0
+    });
+  },
+
+  nextDay() {
+    this.player.tag++;
+  },
+
+  save() {
+    localStorage.setItem("elandor_save", JSON.stringify(this.player));
+  },
+
+  load() {
+    const save = localStorage.getItem("elandor_save");
+    if (save) this.player = JSON.parse(save);
+  }
+};
